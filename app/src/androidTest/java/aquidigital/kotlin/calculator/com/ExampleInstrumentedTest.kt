2841af28ec30f856bee2716af84f7752b179348e
Ruairi McGuigan <r.mcguigan@waracle.com>
2018-04-10 09:35:33 +0100

    calc layout complete

---
+++ app/src/androidTest/java/aquidigital/kotlin/calculator/com/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package aquidigital.kotlin.calculator.com
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("aquidigital.kotlin.calculator.com", appContext.packageName)
+    }
+}
